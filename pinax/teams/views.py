92f1010fdaeb04440227f55dadc61b4e36dd920d
qorbarsi <qorbarsi@gmail.com>
2016-12-13 21:26:29 +0200

    context_instance

--- pinax/teams/views.py
+++ pinax/teams/views.py
@@ -239,7 +239,6 @@ class TeamInviteView(FormView):
                     "invite_form": self.get_unbound_form(),
                     "team": self.team
                 },
-                #context_instance=RequestContext(self.request)
             )
         }
 
@@ -263,7 +262,6 @@ class TeamInviteView(FormView):
                             "membership": membership,
                             "team": self.team
                         },
-                        #context_instance=RequestContext(self.request)
                     )
                 }
             })
@@ -287,7 +285,8 @@ class TeamInviteView(FormView):
             "html": render_to_string("pinax/teams/_invite_form.html", {
                 "invite_form": form,
                 "team": self.team
-            }, #context_instance=RequestContext(self.request))
+            },
+            )
         }
         return self.render_to_response(data)
 
@@ -318,7 +317,6 @@ def team_member_resend_invite(request, pk):
                 "membership": membership,
                 "team": request.team
             },
-            #context_instance=RequestContext(request)
         )
     }
     return HttpResponse(json.dumps(data), content_type="application/json")
@@ -336,7 +334,6 @@ def team_member_promote(request, pk):
                 "membership": membership,
                 "team": request.team
             },
-            #context_instance=RequestContext(request)
         )
     }
     return HttpResponse(json.dumps(data), content_type="application/json")
@@ -354,7 +351,6 @@ def team_member_demote(request, pk):
                 "membership": membership,
                 "team": request.team
             },
-            #context_instance=RequestContext(request)
         )
     }
     return HttpResponse(json.dumps(data), content_type="application/json")
